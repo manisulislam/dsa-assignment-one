2bd1a6b63e1eec3f47e90b17ca9a164fa608f9c7
anisul islam <anisulislam980340@gmail.com>
2023-06-09 00:04:51 +0600

    duplicate

---
+++ duplicate.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int flag=0;
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<=n; j++){
+            if(a[i]==a[j]){
+                flag=1;
+
+            }
+        }
+    }
+    if(flag==1){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
